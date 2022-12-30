cfac59fda844971bdc8e159068c543b37efe2e4a
murphyiank <109882059+murphyiank@users.noreply.github.com>
2022-12-30 08:03:55 -0700

    commit tree 231aa0dc23485c6f6f714f42978771416289919e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const bDay = "surprise";
+function writeCards(names, bDay){
+    let thankYou= []
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${bDay} gift!`);
+        }
+    return thankYou;
+}
+
+
+
+function countDown(){
+    let down = 10;
+    while (down >=0){
+        console.log(down--);
+    }
+}
